5a56a0b9e8cdc113dd13400f633b2890fb02b38a
Jorge Martinez <jorge.marsal@gmail.com>
2015-07-16 06:53:07 -0700

    Add ddc_write() function.

--- ddc/src/RcppExports.cpp
+++ ddc/src/RcppExports.cpp
@@ -5,6 +5,19 @@
 
 using namespace Rcpp;
 
+// ddc_write
+SEXP ddc_write(SEXP object, SEXP url, const Rcpp::List& options);
+RcppExport SEXP ddc_ddc_write(SEXP objectSEXP, SEXP urlSEXP, SEXP optionsSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< SEXP >::type object(objectSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type url(urlSEXP);
+    Rcpp::traits::input_parameter< const Rcpp::List& >::type options(optionsSEXP);
+    __result = Rcpp::wrap(ddc_write(object, url, options));
+    return __result;
+END_RCPP
+}
 // ddc_read
 Rcpp::DataFrame ddc_read(const std::string& url, const Rcpp::List& options);
 RcppExport SEXP ddc_ddc_read(SEXP urlSEXP, SEXP optionsSEXP) {
--- ddc/src/ddc_rbindings.cpp
+++ ddc/src/ddc_rbindings.cpp
@@ -19,13 +19,60 @@ typedef std::map<std::string, boost::any> ConfigurationMap;
 } // namespace base
 
 namespace ddc {
-boost::any ddc_read(const std::string &url,
+boost::any ddc_read(const std::string& url,
                     const std::string& objectType,
                     base::ConfigurationMap& conf);
+void ddc_write(const std::string& url,
+               const std::string& bytes,
+               base::ConfigurationMap& conf);
 }
+
+
+// [[Rcpp::export]]
+SEXP ddc_write(SEXP object,
+               SEXP url,
+               const Rcpp::List& options) {
+    base::ConfigurationMap conf;
+    std::string hdfsConfigurationFile;
+    try {
+        hdfsConfigurationFile =  Rcpp::as<std::string>(options["hdfsConfigurationFile"]);
+        conf["hdfsConfigurationFile"] = hdfsConfigurationFile;
+    }
+    catch(...) {
+        //PASS
+    }
+    bool overwrite = false;
+    try {
+        overwrite =  Rcpp::as<bool>(options["overwrite"]);
+        conf["overwrite"] = overwrite;
+    }
+    catch(...) {
+        //PASS
+    }
+
+    switch(TYPEOF(object)) {
+    case STRSXP: {
+        ddc::ddc_write(CHAR(STRING_ELT(url, 0)), CHAR(STRING_ELT(object, 0)), conf);
+        break;
+    }
+    case RAWSXP: {
+        std::string buffer;
+        buffer.resize(Rf_length(object));
+        memcpy((void *)buffer.data(), RAW(object), Rf_length(object));
+        ddc::ddc_write(CHAR(STRING_ELT(url, 0)), buffer, conf);
+        break;
+    }
+    default:
+        throw std::runtime_error("Unsupported R object. Must be string or raw");
+        break;
+    }
+
+    return R_NilValue;
+}
+
 // [[Rcpp::export]]
 Rcpp::DataFrame ddc_read(const std::string& url,
-                    const Rcpp::List& options)
+                         const Rcpp::List& options)
 {
     base::ConfigurationMap conf;
     //std::vector<uint64_t> offsets;
