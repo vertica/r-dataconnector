165a95db0b1b603e0beb416bd8c83c53775f1130
Jorge Martinez <jorge.marsal@gmail.com>
2015-07-30 09:19:33 -0700

    Rename int64_t type to int64.

--- ddc/tests/testthat/test_csv.R
+++ ddc/tests/testthat/test_csv.R
@@ -1,13 +1,13 @@
 library(ddc)
 
 test_that("ex001.csv", {
-  df <- ddc_read(paste(getwd(),'/../../src/distributed-data-connector/ddc/ddc/test/data/ex001.csv',sep=''), options=list(schema='a:int64_t,b:string'))
+  df <- ddc_read(paste(getwd(),'/../../src/distributed-data-connector/ddc/ddc/test/data/ex001.csv',sep=''), options=list(schema='a:int64,b:string'))
   expect_equal(nrow(df), 3)
   expect_equal(ncol(df), 2)
 })
 
 test_that("ex002.csv", {
-  df <- ddc_read(paste(getwd(),'/../../src/distributed-data-connector/ddc/ddc/test/data/ex002.csv',sep=''), options=list(schema='a:int64_t,b:string,c:int64_t,d:string'))
+  df <- ddc_read(paste(getwd(),'/../../src/distributed-data-connector/ddc/ddc/test/data/ex002.csv',sep=''), options=list(schema='a:int64,b:string,c:int64,d:string'))
   expect_equal(nrow(df), 3)
   expect_equal(ncol(df), 4)
 })
