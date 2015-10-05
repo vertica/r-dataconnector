7409d49f579095894316dff34e82281821e2f576
Jorge Martinez <jms@hpe.com>
2015-10-05 11:23:47 -0700

    Improve profiling script.

--- hdfsconnector/src/distributed-data-connector/ddc/tools/profiler.sh
+++ hdfsconnector/src/distributed-data-connector/ddc/tools/profiler.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-CPUPROFILE=/tmp/$1.prof LD_PRELOAD=/usr/local/lib/libprofiler.so:/usr/local/lib/libtcmalloc.so GLOG_logtostderr=1 GLOG_minloglevel=0 ./$1
-pprof --gv assembler_benchmark /tmp/$1.prof
+# Usage: binary, args, name
+CPUPROFILE=/tmp/$3.prof LD_PRELOAD=/usr/local/lib/libprofiler.so:/usr/lib/libtcmalloc.so GLOG_logtostderr=1 GLOG_minloglevel=0 "$1" "$2"
+pprof --gv "$1" /tmp/$3.prof
