c15ca9ac1ec4e3cc58f98d02942c995a412bc284
Jorge Martinez <jms@hpe.com>
2015-10-08 08:34:06 -0700

    Fix synthetic CSV creation script.

--- hdfsconnector/src/distributed-data-connector/ddc/tools/create_test_csv_files.sh
+++ hdfsconnector/src/distributed-data-connector/ddc/tools/create_test_csv_files.sh
@@ -5,3 +5,4 @@ python ../tools/create_csv_files.py 4 16 15 ../ddc/test/data/test4MB.offsetcsv 3
 python ../tools/create_csv_files.py 512 128 15 ../ddc/test/data/test512MB.csv 32
 python ../tools/create_csv_files.py 512 128 15 ../ddc/test/data/test512MB.offsetcsv 32
 python ../tools/create_csv_files.py 512 10 15 ../ddc/test/data/test512MB_10col.csv 32
+echo "000000033554432,000000033554433,000000033554434,000000033554435,000000033554436,000000033554437,000000033554438,000000033554439" >> ../ddc/test/data/test512MB_10col.csv
