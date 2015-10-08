373f6f7442d206e0620addf97deb01eb11d3d5f1
Jorge Martinez <jms@hpe.com>
2015-10-07 17:17:37 -0700

    Add test file.

--- hdfsconnector/src/distributed-data-connector/ddc/tools/create_test_csv_files.sh
+++ hdfsconnector/src/distributed-data-connector/ddc/tools/create_test_csv_files.sh
@@ -4,3 +4,4 @@ python ../tools/create_csv_files.py 4 16 15 ../ddc/test/data/test4MB.csv 32
 python ../tools/create_csv_files.py 4 16 15 ../ddc/test/data/test4MB.offsetcsv 32
 python ../tools/create_csv_files.py 512 128 15 ../ddc/test/data/test512MB.csv 32
 python ../tools/create_csv_files.py 512 128 15 ../ddc/test/data/test512MB.offsetcsv 32
+python ../tools/create_csv_files.py 512 10 15 ../ddc/test/data/test512MB_10col.csv 32
