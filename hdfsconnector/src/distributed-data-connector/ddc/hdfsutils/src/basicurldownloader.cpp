a08b40023bc4f605e14ba8b6bdcff06b6cdc64ea
Jorge Martinez <jms@hpe.com>
2015-10-14 11:38:02 -0700

    Add Apache 2.0 license.

--- hdfsconnector/src/distributed-data-connector/ddc/hdfsutils/src/basicurldownloader.cpp
+++ hdfsconnector/src/distributed-data-connector/ddc/hdfsutils/src/basicurldownloader.cpp
@@ -1,3 +1,20 @@
+/*
+(c) Copyright 2015 Hewlett Packard Enterprise Development LP
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
 #include "basicurldownloader.h"
 #include <string.h>
 #include <boost/format.hpp>
@@ -146,7 +163,7 @@ int BasicUrlDownloader::downloadSpeedLimit(const std::string &url, long lowSpeed
         prog.curl = curl_;
 
         //curl_easy_setopt(curl, CURLOPT_URL, "http://example.com/");
-        DLOG(INFO) << "downloading: " << url;
+        DLOG(INFO) << "id: " << this << " downloading: " << url;
         curl_easy_setopt(curl_, CURLOPT_URL, url.c_str());
 
         curl_easy_setopt(curl_, CURLOPT_BUFFERSIZE, 1*1024*1024);  // TODO magic
--- hdfsconnector/src/distributed-data-connector/ddc/tools/license.py
+++ hdfsconnector/src/distributed-data-connector/ddc/tools/license.py
@@ -1,4 +1,4 @@
-/*
+'''
 (c) Copyright 2015 Hewlett Packard Enterprise Development LP
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,14 +12,24 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-*/
 
 
-#ifndef DDC_GLOBALS_H
-#define DDC_GLOBALS_H
+$ cat script.sh 
+for i in $* # or whatever other pattern...
+do
+  if ! grep -q Copyright $i
+  then
+    cat LICENSE $i >$i.new && mv $i.new $i
+  fi
+done
 
-namespace ddc {
-    extern volatile bool stopDdc;
-} // namespace ddc
-
-#endif // DDC_GLOBALS_H
+dirs = 'assembler base blockreader ddc distributor hdfsutils recordparser scheduler splitproducer worker'.split(' ')
+dirs2 = ['hdfsconnector/src/distributed-data-connector/ddc/' + dir for dir in dirs]
+pat = ['/*.h','/*.cpp']
+dirs3 = []
+for d in dirs2:
+    for subd in ['/src/','/./','/test/']:
+        for p in pat:
+            dirs3.append(d+subdir+p)
+print ' '.join(dirs3)
+'''
